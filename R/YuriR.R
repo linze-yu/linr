d8b00c1d6b97cfb0cf6da4faf6a2c228a5eab629
Yuri <69614723+Yurisuriyel@users.noreply.github.com>
2022-03-07 23:18:24 +0800

    Add files via upload

--- R/YuriR.R
+++ R/YuriR.R
@@ -81,55 +81,56 @@ outer.IQR <- function(x, multiple = 1.5, replace = FALSE, revalue = NA) {
   else outlier <- data.table(location = 0, value = 0)
   if (replace == TRUE) {
     x[x1] <- revalue
-    }
-  return(list(new.value = x, outlier = outlier))
   }
+  return(list(new.value = x, outlier = outlier))
+}
 
 #' @export Yuri_theme
 Yuri_theme <- theme(text = element_text(color = 1), #
-                 plot.title = element_text(hjust = 0.5, angle = 0, lineheight = 1, margin = margin(0, 1, 0, 0)), #
-                 plot.subtitle = element_text(angle = 0, lineheight = 1, margin = margin(0, 0, 2, 0)), #
-                 plot.caption = element_text(angle = 0, lineheight = 1, margin = margin(0, 0, 0, 0)), #
-                 plot.tag = element_text(angle = 0, lineheight = 1, margin = margin(0, 0, 0, 0)), #
-                 legend.title = element_text(margin = margin(0, 0, 0, 0)), #
-                 legend.text = element_text(margin = margin(0, 0, 0, 0)), #
-                 strip.text = element_text(hjust = 0, margin = margin(0, 0, 0, 0)), #
-                 axis.text.y = element_text(vjust = 0.5, hjust = 0.5, angle = 0, margin = margin(0, 1, 0, 0)), #
-                 axis.title.y = element_text(vjust = 0.5, hjust = 0.5, angle = 90, margin = margin(0, 3, 0, 0)), #
-                 axis.text.x = element_text(vjust = 0.5, hjust = 0.5, angle = 0, margin = margin(1, 0, 0, 0)), #
-                 axis.title.x = element_text(vjust = 0.5, hjust = 0.5, angle = 0, margin = margin(2, 0, 0, 0)), #
-                 panel.background = element_rect(fill = NA), #
-                 panel.border = element_rect(color = 1, fill = NA, size = 1), #
-                 plot.background = element_rect(fill = NA, color = NA), #
-                 legend.background = element_rect(fill = NA, color = NA), #
-                 legend.key = element_rect(fill = NA, color = NA), #
-                 strip.background = element_rect(fill = NA), #
-                 axis.line = element_line(color = 1, size = 1, lineend = "square"), #
-                 axis.ticks.x = element_line(color = 1, size = 1), #
-                 axis.ticks.y = element_line(color = 1, size = 1), #
-                 #axis.ticks.length = unit(.15, "cm"), #
-                 panel.grid.major.x = element_line(color = "#96999C", size = 0.5, linetype = 3), #
-                 panel.grid.major.y = element_line(color = "#96999C", size = 0.5, linetype = 3), #
-                 panel.grid.minor.x = element_line(color = "#96999C", size = 0.25, linetype = 3), #
-                 panel.grid.minor.y = element_line(color = "#96999C", size = 0.25, linetype = 3), #
-                 plot.margin = margin(t = 0.5, r = 0.5, b = 0.1, l = 0.5, unit = "cm"), #
-                 #panel.spacing = unit(.25, "cm"), #
-                 legend.margin = margin(t = 0, r = 0, b = 0, l = -9), #
-                 legend.position = ("right"), #
-                 #legend.key.height = unit(0.5, "cm"), #
-                 #legend.key.width = unit(0.5, "cm"), #
-                 #legend.key.size = unit(0.5, "cm"), #
-                 legend.title.align = (0), #
-                 legend.text.align = (0), #
-                 legend.direction = "vertical", #
-                 legend.justification = c(1, 1), #
-                 legend.box = "horizontal", #
-                 legend.box.just = (0.5), #
-                 plot.caption.position = "panel",
-                 plot.tag.position = "topleft",
-                 plot.title.position = "plot",
-                 #legend.key = element_rect(fill = "group", color = "group"), #
+                    plot.title = element_text(hjust = 0.5, angle = 0, lineheight = 1, margin = margin(0, 1, 0, 0)), #
+                    plot.subtitle = element_text(angle = 0, lineheight = 1, margin = margin(0, 0, 2, 0)), #
+                    plot.caption = element_text(angle = 0, lineheight = 1, margin = margin(0, 0, 0, 0)), #
+                    plot.tag = element_text(angle = 0, lineheight = 1, margin = margin(0, 0, 0, 0)), #
+                    legend.title = element_text(margin = margin(0, 0, 0, 0)), #
+                    legend.text = element_text(margin = margin(0, 0, 0, 0)), #
+                    strip.text = element_text(hjust = 0, margin = margin(0, 0, 0, 0)), #
+                    axis.text.y = element_text(vjust = 0.5, hjust = 0.5, angle = 0, margin = margin(0, 1, 0, 0)), #
+                    axis.title.y = element_text(vjust = 0.5, hjust = 0.5, angle = 90, margin = margin(0, 3, 0, 0)), #
+                    axis.text.x = element_text(vjust = 0.5, hjust = 0.5, angle = 0, margin = margin(1, 0, 0, 0)), #
+                    axis.title.x = element_text(vjust = 0.5, hjust = 0.5, angle = 0, margin = margin(2, 0, 0, 0)), #
+                    panel.background = element_rect(fill = NA), #
+                    panel.border = element_rect(color = 1, fill = NA, size = 1), #
+                    plot.background = element_rect(fill = NA, color = NA), #
+                    legend.background = element_rect(fill = NA, color = NA), #
+                    legend.key = element_rect(fill = NA, color = NA), #
+                    strip.background = element_rect(fill = NA), #
+                    axis.line = element_line(color = 1, size = 1, lineend = "square"), #
+                    axis.ticks.x = element_line(color = 1, size = 1), #
+                    axis.ticks.y = element_line(color = 1, size = 1), #
+                    #axis.ticks.length = unit(.15, "cm"), #
+                    panel.grid.major.x = element_line(color = "#96999C", size = 0.5, linetype = 3), #
+                    panel.grid.major.y = element_line(color = "#96999C", size = 0.5, linetype = 3), #
+                    panel.grid.minor.x = element_line(color = "#96999C", size = 0.25, linetype = 3), #
+                    panel.grid.minor.y = element_line(color = "#96999C", size = 0.25, linetype = 3), #
+                    plot.margin = margin(t = 0.5, r = 0.5, b = 0.1, l = 0.5, unit = "cm"), #
+                    #panel.spacing = unit(.25, "cm"), #
+                    legend.margin = margin(t = 0, r = 0, b = 0, l = -9), #
+                    legend.position = ("right"), #
+                    #legend.key.height = unit(0.5, "cm"), #
+                    #legend.key.width = unit(0.5, "cm"), #
+                    #legend.key.size = unit(0.5, "cm"), #
+                    legend.title.align = (0), #
+                    legend.text.align = (0), #
+                    legend.direction = "vertical", #
+                    legend.justification = c(1, 1), #
+                    legend.box = "horizontal", #
+                    legend.box.just = (0.5), #
+                    plot.caption.position = "panel",
+                    plot.tag.position = "topleft",
+                    plot.title.position = "plot",
+                    #legend.key = element_rect(fill = "group", color = "group"), #
 )
 
 #' @export Up
 Up <- print("rvcheck::update_all()")
+
