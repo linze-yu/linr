7a08ef11335ce2e7bd6e7d67ea92572860255531
linze <69614723+Yurisuriyel@users.noreply.github.com>
2022-05-20 20:26:23 +0800

    Add files via upload

--- R/YuriR.R
+++ R/YuriR.R
@@ -1,14 +1,12 @@
-#' @title Yuri
+#' @title linr
 #'
-#' @description cols, fills, t_theme, Yuri_theme, stdca, dca, Up, cclean
+#' @description cols, fills, lin_theme, stdca, dca, Up, cclean
 #'
 #' @param cols
 #'
 #' @param fills
 #'
-#' @param t_theme
-#'
-#' @param Yuri_theme
+#' @param lin_theme
 #'
 #' @param stdca
 #'
@@ -28,58 +26,8 @@ cols <- c("#696AAD","#FC766A","#00997B","#EBBF57","#C34E7C","#34558B","#BD3645",
 #' @export fills
 fills <- c("#696AAD","#FC766A","#00997B","#EBBF57","#C34E7C","#34558B","#BD3645","#92B558","#F3E04E","#6B5B95","#95AAD3","#E34F33","#4DB6AD","#DECEBB","#975A58","#6364A5","#9D363F","#5BB3B0","#E2654D","#D85A7B","#98B2D1","#F8CDCD","#7FC9CB","#B067A1")
 
-#' @export t_theme
-t_theme <- theme(
-  text = element_text(family = "Source Han Sans CN-Regular", color = 1), # 全字体
-  plot.title = element_text(size = 23, hjust = 0.5, angle = 0, lineheight = 1, margin = margin(0, 1, 0, 0)), # 标题
-  plot.subtitle = element_text(size = 17, angle = 0, lineheight = 1, margin = margin(0, 0, 2, 0)), # 副标题
-  plot.caption = element_text(size = 14, angle = 0, lineheight = 1, margin = margin(0, 0, 0, 0)), # 说明文字
-  plot.tag = element_text(size = 15, angle = 0, lineheight = 1, margin = margin(0, 0, 0, 0)), # 标签
-  legend.title = element_text(size = 12, margin = margin(0, 0, 0, 0)), # 图例标题
-  legend.text = element_text(size = 12, margin = margin(0, 0, 0, 0)), # 图例文字
-  strip.text.x = element_text(size = 15, hjust = 0.5, margin = margin(0, 0, 0, 0)), # 水平分面标签文本
-  strip.text.y = element_text(size = 15, vjust = 0.5, margin = margin(0, 0, 0, 0)), # 竖直分面标签文本
-  axis.text.y = element_text(size = 14, vjust = 0.5, hjust = 1, angle = 0, margin = margin(0, 1, 0, 0)), # Y轴数字标
-  axis.title.y = element_text(size = 16, vjust = 0.5, hjust = 0.5, angle = 90, margin = margin(0, 3, 0, 0)), # Y轴标题
-  axis.text.x = element_text(size = 14, vjust = 0.5, hjust = 0.5, angle = 0, margin = margin(1, 0, 0, 0)), # X轴数字标
-  axis.title.x = element_text(size = 15, vjust = 0.5, hjust = 0.5, angle = 0, margin = margin(2, 0, 0, 0)), # X轴标题
-  panel.background = element_rect(fill = NA), # 绘图区域背景
-  panel.border = element_rect(color = 1, fill = NA, size = 1), # 绘图区边框
-  plot.background = element_rect(fill = NA, color = NA), # 文字区域+图像背景
-  legend.background = element_rect(fill = NA, color = NA), # 图例背景
-  legend.key = element_rect(fill = NA, color = NA), # 图例符号背景
-  strip.background = element_rect(fill = NA), # 分面标签背景
-  axis.line = element_line(color = 1, size = 1, lineend = "square"), # 坐标轴
-  axis.ticks.x = element_line(color = 1, size = 0.5), # X轴刻度线
-  axis.ticks.y = element_line(color = 1, size = 0.5), # Y轴刻度线
-  axis.ticks.length = unit(.15, "cm"), # 刻度线长度
-  panel.grid.major.x = element_line(color = "#96999C", size = 0.5, linetype = 3), # 主竖线
-  panel.grid.major.y = element_line(color = "#96999C", size = 0.5, linetype = 3), # 主横线
-  panel.grid.minor.x = element_line(color = "#96999C", size = 0.25, linetype = 3), # 次竖线
-  panel.grid.minor.y = element_line(color = "#96999C", size = 0.25, linetype = 3), # 次横线
-  plot.margin = margin(t = 0.5, r = 0.5, b = 0.1, l = 0.5, unit = "cm"), # 图边界(距离可以为0或者为负值)
-  panel.spacing.x = unit(.25, "cm"), # 竖直分面绘图区之间的间距
-  panel.spacing.y = unit(.25, "cm"), # 水平分面绘图区之间的间距
-  legend.margin = margin(t = 0, r = 0, b = 0, l = -9), # 图例边界, unit = "cm"
-  legend.position = ("right"), # 图例位置left, right, bottom, top, c(0.9, 0.9)
-  legend.key.height = unit(0.5, "cm"), # 图例符号高度
-  legend.key.width = unit(0.5, "cm"), # 图例符号宽度
-  legend.key.size = unit(0.5, "cm"), # 图例符号大小
-  legend.title.align = (0), # 图例标题对齐方式(0为左齐, 1为右齐, 0.5居中)
-  legend.text.align = (0), # 图例文字标签对齐方式(0为左齐, 1为右齐, 0.5居中)
-  legend.direction = "vertical", # 图例排列方向"horizontal"(水平一行)
-  legend.justification = c(1, 1), # 图例位置	center或两数字向量
-  legend.box = "vertical", # 多图例的排列方式"horizontal"
-  legend.box.just = (0.5), # 多图例居中方式
-  plot.caption.position = "panel",
-  plot.tag.position = "topleft",
-  plot.title.position = "plot",
-  # aspect.ratio(7/5),
-  # legend.key = element_rect(fill = "group", color = "group"),#图例符号
-)
-
-#' @export Yuri_theme
-Yuri_theme <- theme(#Source Han Sans CN-Regular
+#' @export lin_theme
+lin_theme <- theme(
   text = element_text(family = "Source Han Sans CN-Regular", color = 1), # 全字体
   plot.title = element_text(size = 12, hjust = 0.5, angle = 0, lineheight = 1, margin = margin(0, 1, 0, 0)), # 标题
   plot.subtitle = element_text(angle = 0, lineheight = 1, margin = margin(0, 0, 2, 0)), # 副标题
